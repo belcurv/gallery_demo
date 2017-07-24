7178484049ae57cef54732366b40a2d0dc7cb2f4
Jay Schwane <jrschwane@uwalumni.com>
2017-07-24 10:28:00 -0500

    Refactor repeated html utility out of templates and into util module

--- src/js/templates/about.tpl.js
+++ src/js/templates/about.tpl.js
@@ -1,25 +1,6 @@
 /* jshint esversion:6 */
 
-/* utility method for easy templating of repeating html elements
- *
- * @params  [array]  literalsArr   [array of all the literal secti
- * @params  [array]  ...cooked     [rest param: all the proccessed expressions]
- * @returns [string]               [the processed string]
-*/
-const html = (literalsArr, ...cooked) => {
-    let result = '';
-    
-    cooked.forEach( (cook, indx) => {
-        let lit = literalsArr[indx];
-        if (Array.isArray(cook)) {
-            cook = cook.join('');
-        }
-        result += lit;
-        result += cook;
-    });
-    result += literalsArr[literalsArr.length - 1];
-    return result;
-};
+import { html } from '../util';
 
 
 /* generate gallery link
